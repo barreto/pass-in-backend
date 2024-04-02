9ac16da4cfa71c66af4a71c1e8859164644b7b49
Barreto <v.barreto.and@gmail.com>
2024-04-01 22:50:15 -0300

    add initial structure

---
+++ src/test/java/com/rocketseat/nlw/passin/PassInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.nlw.passin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
