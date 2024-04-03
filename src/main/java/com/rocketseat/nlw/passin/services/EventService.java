f1eff2420766c6af9d56e06a3e9482f109216516
Barreto <v.barreto.and@gmail.com>
2024-04-02 23:41:11 -0300

    improve dependency declaration with lombok annotation

--- src/main/java/com/rocketseat/nlw/passin/services/EventService.java
+++ src/main/java/com/rocketseat/nlw/passin/services/EventService.java
@@ -1,13 +1,11 @@
 package com.rocketseat.nlw.passin.services;
 
 import com.rocketseat.nlw.passin.repositories.EventRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
-
-    public EventService(EventRepository eventRepository) {
-        this.eventRepository = eventRepository;
-    }
 }
